d51fef903cd3d10e25fa6ca94b6f01fd5d8f3483
mflec <mifilechares@gmail.com>
2021-08-01 15:21:03 -0300

    modified app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Home from "./views/Home";
 import { Route, Switch } from "react-router-dom";
 import './styles/App.css';
@@ -6,19 +7,16 @@ import Add from "./views/Add";
 import LandingPage from "./views/LandingPage";
 import Error404 from "./views/Error404";
 
-
 function App() {
   return (
-    <div>
-      <div className="container content">
-        <Switch>
-      <Route exact path="/" component={LandingPage}/>
-        <Route path="/home" exact component={Home}/>
-        <Route path="/videogame" exact component={Add}/>
-        <Route path="/videogame/:idGame"  exact component={Videogame}/>
+    <div className="container content">
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/videogame" exact component={Add} />
+        <Route path="/videogame/:idGame" exact component={Videogame} />
         <Route path="*" component={Error404} />
-        </Switch>
-      </div>
+      </Switch>
     </div>
   );
 };
