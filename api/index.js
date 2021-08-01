084b298b98ada7f53fe4725cfacec512130e469f
mflec <mifilechares@gmail.com>
2021-08-01 13:48:42 -0300

    set genres to the db

--- api/index.js
+++ api/index.js
@@ -3,12 +3,10 @@ const { conn, Genre } = require('./src/db.js');
 const axios = require('axios').default;
 require("dotenv").config();
 
-const PORT = 3005;
-
 const RAWG_GENRES = `https://api.rawg.io/api/genres`;
 const KEY = "0a54940f8789456aa6d405ce652df399";
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
     app.listen(process.env.PORT, async() => {
         let apiGenres = await axios.get(`${RAWG_GENRES}?key=${KEY}`);
         apiGenres = await apiGenres.data.results.map(genre => Genre.create({
