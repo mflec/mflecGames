3aff9cdb26a402fb8c1c031eeea7bb4579bd89af
mflec <mifilechares@gmail.com>
2021-08-01 13:45:49 -0300

    force in false

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const PORT = 3005;
 /*const RAWG_GENRES = `https://api.rawg.io/api/genres`;
 const KEY = "0a54940f8789456aa6d405ce652df399";*/
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: false }).then(() => {
     app.listen(process.env.PORT, async() => {/*
         let apiGenres = await axios.get(`${RAWG_GENRES}?key=${KEY}`);
         apiGenres = await apiGenres.data.results.map(genre => Genre.create({
