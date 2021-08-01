359e4254597f6d9cfbeb3d1d9296c64d1e82f0a0
mflec <mifilechares@gmail.com>
2021-08-01 13:47:10 -0300

    adsdas

--- api/index.js
+++ api/index.js
@@ -5,16 +5,16 @@ require("dotenv").config();
 
 const PORT = 3005;
 
-/*const RAWG_GENRES = `https://api.rawg.io/api/genres`;
-const KEY = "0a54940f8789456aa6d405ce652df399";*/
+const RAWG_GENRES = `https://api.rawg.io/api/genres`;
+const KEY = "0a54940f8789456aa6d405ce652df399";
 
 conn.sync({ force: false }).then(() => {
-    app.listen(process.env.PORT, async() => {/*
+    app.listen(process.env.PORT, async() => {
         let apiGenres = await axios.get(`${RAWG_GENRES}?key=${KEY}`);
         apiGenres = await apiGenres.data.results.map(genre => Genre.create({
         name: genre.name,
          id: genre.id
-        }));*/
+        }));
         console.log(`Listening in port ${process.env.PORT}`)
     })
 });
