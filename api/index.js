53aec7fbdffd53170024837096299851de1055d6
mflec <mifilechares@gmail.com>
2021-08-01 17:08:15 -0300

    unnecessary data changed

--- api/index.js
+++ api/index.js
@@ -1,20 +1,8 @@
 const app = require("./src/app");
 const { conn} = require('./src/db.js');
 
-// const {Genre} = require('./src/db.js');
-//const axios = require('axios').default;
-
-
-//const RAWG_GENRES = `https://api.rawg.io/api/genres`;
-//const KEY = "0a54940f8789456aa6d405ce652df399";
-
 conn.sync({ force: false }).then(() => {
     app.listen(process.env.PORT, () => {
-        /*let apiGenres = await axios.get(`${RAWG_GENRES}?key=${KEY}`);
-        apiGenres = await apiGenres.data.results.map(genre => Genre.create({
-        name: genre.name,
-         id: genre.id
-        }));*/
-        console.log(`Listening in port`)
+        console.log(`Listening...`)
     })
 });
--- api/src/db.js
+++ api/src/db.js
@@ -33,13 +33,6 @@ let sequelize =
         { logging: false, native: false }
       );
 
-
-
-//const sequelize = new Sequelize(`postgres://postgres:40644938@localhost:3000/videogames`, {
-//    logging: false, // set to console.log to see the raw SQL queries
-//    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//});
-
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
